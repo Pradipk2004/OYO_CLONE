a6b843aa7843b4fa8f207d01857d792311606737
Pradip Kanawade <your-email@example.com>
2025-07-08 16:21:21 +0530

    Temp: create superuser

--- home/views.py
+++ home/views.py
@@ -56,13 +56,13 @@ def hotel_details(request, slug):
     return render(request, 'hotel_detail.html', context = {'hotel' : hotel})
 
 
-from django.contrib.auth import get_user_model
+#from django.contrib.auth import get_user_model
 
-User = get_user_model()
+#User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="pradipkanawade07@gmail.com",
-        password="admin123"
-    )
+#if not User.objects.filter(username="admin").exists():
+#    User.objects.create_superuser(
+#        username="admin",
+#        email="pradipkanawade07@gmail.com",
+#        password="admin123"
+#    )
